29aba980f350d37f23ccc24859ce8f9d936216be
gazievDima <dmtr.cw@gmail.com>
2022-04-19 13:24:02 +0300

    ..

--- src/main/kotlin/task/Task.kt
+++ src/main/kotlin/task/Task.kt
@@ -1,21 +1,41 @@
 package operators.task
-
-//задача по операторам rxjava2
-
 /*
 
-задача: получаем объект с апи ProductApi с полями:
-    id, name, price, type, description
-    вывести товары по группам.
+we have model Product(id: Int, name: String, price: Int, description: String)
+we have list products from server
+we have list products from cache (local database)
 
-- убрать дубли
-- преобразовать в GetProducts (в случае если приходит ценой null то изменить description на "нет товара")
-- отфильтровать по увеличению цены
-- разбить по типам (типы food, non-food)
+task sources:
+- RemoteProductsService.kt, получить список продуктов с удаленного сервера
+    -api:
+        get(): Single<List<ProductRemoteData>>
+- LocalProductsService.kt, get list products from cache (локальная бд)
+    -api:
+        get(): Single<List<ProductLocalData>>
+
+task repository:
+- GetListProductsRepository.kt, get list products
+    -api:
+        getListProducts(): Single<List<Product>> - merge 2 flow from sources in one flow no repetitive
+
+task use-cases:
+- GetListProductsUseCase.kt, возвращает список продуктов по условию, разбитый по non-food, food
+    -api:
+       getProducts(): List<Product> - get list products from sources without filtration
+       getSortedProductsPriceComparator.up(): List<Product> - get sorted list to price from min to max
+       getSortedProductsPriceComparator.down(): List<Product> - get sorted list to price from max to min
+       getSortedProductsNameComparator.up(): List<Product> - get sorted list to name from a to z
+       getSortedProductsNameComparator.down(): List<Product> - get sorted list to name from z to a
+       getProductsFilterPrice.filterUp(minPrice: Int, maxPrice): List<Product> - get products with filter price from min to max
+       getProductsFilterPrice.filterDown(minPrice: Int, maxPrice): List<Product> - get products with filter price from max to min
+
+task presenter:
+- Presenter.kt, used use-cases for get list products
+
+task view:
+- View.kt, client code for used Presenter.kt
 
  */
 
 fun main() {
-    val repository = GetProductsRepository()
-    //repository.get()
 }
